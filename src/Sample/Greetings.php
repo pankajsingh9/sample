12ce72bcd84262ee5f753a8a9f6d3bf9f8c4a314
Pankaj <pankajaswalranikhet@gmail.com>
2020-09-21 11:18:05 +0530

    new amazon changes updated

--- src/Sample/Greetings.php
+++ src/Sample/Greetings.php
@@ -41,6 +41,27 @@ class Greetings {
     public static function sayHelloWorld()
     {
 
+
+        $amazon = [
+            'EU' => [
+                'dev_id' => 233623308975,
+                'access_key' => 'AKIAJPQEXA5OEL5OK5QQ',
+                'secret_key' => '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'
+            ],
+            'NA' => [
+                'dev_id' => 337320726556,
+                'access_key' => 'AKIAJREM35VIGP7YALWA',
+                'secret_key' => '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'
+            ],
+            'FE' => [
+                'dev_id' => 716069427621,
+                'access_key' => 'AKIAI5IWWEKODV46Z4GA',
+                'secret_key' => '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'
+            ]
+        ];
+
+        echo base64_encode(json_encode($amazon));die('test');
+
         $sdk = new \Aws\Sdk([
             'region'   => 'us-east-2',
             'version'  => 'latest',
